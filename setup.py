eaef79904119b87c696c3f7609b708bebd2dcdb5
Simo Sorce <simo@redhat.com>
2015-05-27 20:18:59 -0400

    Fix packages in setup.py
    Signed-off-by: Simo Sorce <simo@redhat.com>

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     maintainer = 'Custodia project Contributors',
     maintainer_email = 'simo@redhat.com',
     url='https://github.com/simo5/custodia',
-    packages = ['custodia', 'custodia.http'],
+    packages = ['custodia', 'custodia.httpd', 'custodia.store'],
     data_files = [('share/man/man7', ["man/custodia.7"]),
                   ('share/doc/custodia', ['COPYING', 'README']),
                   ('share/doc/custodia/examples', ['custodia.conf']),
