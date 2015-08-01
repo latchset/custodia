d0e0407e37f85ff086f5f655bf5ce6c59d8e89e2
Simo Sorce <simo@redhat.com>
2015-08-01 15:48:16 -0400

    Release version 0.1.0

--- setup.py
+++ setup.py
@@ -6,14 +6,15 @@ from distutils.core import setup
 
 setup(
     name = 'custodia',
-    version = '0.0.1',
+    version = '0.1.0',
     license = 'GPLv3+',
     maintainer = 'Custodia project Contributors',
     maintainer_email = 'simo@redhat.com',
     url='https://github.com/simo5/custodia',
-    packages = ['custodia', 'custodia.httpd', 'custodia.store'],
+    packages = ['custodia', 'custodia.httpd', 'custodia.store',
+                'custodia.message', 'tests'],
     data_files = [('share/man/man7', ["man/custodia.7"]),
-                  ('share/doc/custodia', ['LICENSE', 'README']),
+                  ('share/doc/custodia', ['LICENSE', 'README', 'API.md']),
                   ('share/doc/custodia/examples', ['custodia.conf']),
                  ],
     scripts = ['custodia/custodia']
--- tests/custodia.py
+++ tests/custodia.py
@@ -4,7 +4,6 @@ from __future__ import absolute_import
 from tests.client import LocalConnection
 import json
 import os
-import signal
 import subprocess
 import time
 import unittest
