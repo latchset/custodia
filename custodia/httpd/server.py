latchset/custodia

commit 57686114cfd464bb6b8a2fe6be6d80ea38d43da9

Allow to send raw data for the simple type
In the simple type case we can allow clients to send the raw value and
request the raw value by setting the application/octet-stream content
type in the Content-Type header for PUTs and the Accept header for GETs.

The content is arbitrary and will be saved by base64 encoding it on receipt.
If the default 'application/json' type is used to later fetch the secret,
then the value field in the json payload will be a base64 string.

Fixes #10

Signed-off-by: Simo Sorce <simo@redhat.com>

custodia/httpd/server.py
@@ -278,7 +278,12 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
                 shutil.copyfileobj(output, self.wfile)
                 output.close()
             elif output is not None:
-                self.wfile.write(str(output).encode('utf-8'))
+                ctype = response.get('headers', {}).get('Content-type',
+                                                        'application/json')
+                if ctype == 'application/octet-stream':
+                    self.wfile.write(bytes(output))
+                else:
+                    self.wfile.write(str(output).encode('utf-8'))
             else:
                 self.close_connection = 1
             self.wfile.flush()

custodia/message/common.py
@@ -44,6 +44,7 @@ class MessageHandler(object):
         self.req = request
         self.name = None
         self.payload = None
+        self.msg_type = None
 
     def parse(self, msg, name):
         """Parses the message.

custodia/message/kem.py
@@ -176,8 +176,9 @@ class KEMHandler(MessageHandler):
         check_kem_claims(claims, name)
         self.name = name
         self.payload = claims.get('value')
+        self.msg_type = 'kem'
 
-        return {'type': 'kem',
+        return {'type': self.msg_type,
                 'value': {'kid': self.client_keys[KEY_USAGE_ENC].key_id,
                           'claims': claims}}
 

custodia/message/simple.py
@@ -14,8 +14,8 @@ class SimpleKey(MessageHandler):
     def parse(self, msg, name):
         """Parses a simple message
 
-        :param req: ignored
         :param msg: the json-decoded value
+        :param name: the requested name
 
         :raises UnknownMessageType: if the type is not 'simple'
         :raises InvalidMessage: if the message cannot be parsed or validated
@@ -30,11 +30,12 @@ class SimpleKey(MessageHandler):
 
         self.name = name
         self.payload = msg
+        self.msg_type = 'simple'
 
     def reply(self, output):
         if self.name.endswith('/'):
             # directory listings are pass-through with simple messages
             return output
 
-        return json.dumps({'type': 'simple', 'value': output},
+        return json.dumps({'type': self.msg_type, 'value': output},
                           separators=(',', ':'))

custodia/secrets.py
@@ -5,6 +5,8 @@ import logging
 import os
 import unittest
 
+from base64 import b64decode, b64encode
+
 from custodia import log
 from custodia.httpd.authorizers import UserNameSpace
 from custodia.httpd.consumer import HTTPConsumer
@@ -56,6 +58,14 @@ class Secrets(HTTPConsumer):
             query = {'type': 'simple', 'value': ''}
         return self._parse(request, query, name)
 
+    def _parse_bin_body(self, request, name):
+        body = request.get('body')
+        if body is None:
+            raise HTTPError(400)
+        value = b64encode(bytes(body)).decode('utf-8')
+        payload = {'type': 'simple', 'value': value}
+        return self._parse(request, payload, name)
+
     def _parse_body(self, request, name):
         body = request.get('body')
         if body is None:
@@ -92,6 +102,29 @@ class Secrets(HTTPConsumer):
             return True
 
         return False
+
+    def _format_reply(self, request, response, handler, output):
+        reply = handler.reply(output)
+        # special case to allow *very* simple clients
+        if handler.msg_type == 'simple':
+            binary = False
+            accept = request.get('headers', {}).get('Accept', None)
+            if accept is not None:
+                types = accept.split(',')
+                for t in types:
+                    if t.strip() == 'application/json':
+                        binary = False
+                        break
+                    elif t.strip() == 'application/octet-stream':
+                        binary = True
+            if binary is True:
+                unwind = json.loads(reply)
+                response['headers'][
+                    'Content-type'] = 'application/octet-stream'
+                response['output'] = b64decode(unwind['value'])
+                return
+
+        response['output'] = reply
 
     def GET(self, request, response):
         trail = request.get('trail', [])
@@ -218,7 +251,7 @@ class Secrets(HTTPConsumer):
     def _int_get_key(self, trail, request, response):
         try:
             name = '/'.join(trail)
-            msg = self._parse_query(request, name)
+            handler = self._parse_query(request, name)
         except Exception as e:
             raise HTTPError(406, str(e))
         key = self._db_key(trail)
@@ -226,7 +259,7 @@ class Secrets(HTTPConsumer):
             output = self.root.store.get(key)
             if output is None:
                 raise HTTPError(404)
-            response['output'] = msg.reply(output)
+            self._format_reply(request, response, handler, output)
         except CSStoreError:
             raise HTTPError(500)
 
@@ -235,13 +268,17 @@ class Secrets(HTTPConsumer):
                     self._int_set_key, trail, request, response)
 
     def _int_set_key(self, trail, request, response):
-        content_type = request.get('headers',
-                                   dict()).get('Content-Type', '')
-        if content_type.split(';')[0].strip() != 'application/json':
-            raise HTTPError(400, 'Invalid Content-Type')
         try:
             name = '/'.join(trail)
-            msg = self._parse_body(request, name)
+
+            content_type = request.get('headers', {}).get('Content-Type', '')
+            content_type_value = content_type.split(';')[0].strip()
+            if content_type_value == 'application/octet-stream':
+                msg = self._parse_bin_body(request, name)
+            elif content_type_value == 'application/json':
+                msg = self._parse_body(request, name)
+            else:
+                raise ValueError('Invalid Content-Type')
         except UnknownMessageType as e:
             raise HTTPError(406, str(e))
         except UnallowedMessage as e:
@@ -586,3 +623,28 @@ class SecretsTests(unittest.TestCase):
         with self.assertRaises(HTTPError) as err:
             self.DELETE(req, rep)
         self.assertEqual(err.exception.code, 409)
+
+    def test_9_0_PUTRawKey(self):
+        req = {'headers': {'Content-Type': 'application/octet-stream'},
+               'remote_user': 'test',
+               'trail': ['test', 'rawkey'],
+               'body': '1234'.encode('utf-8')}
+        rep = {}
+        self.PUT(req, rep)
+
+    def test_9_1_GETRawKey(self):
+        req = {'headers': {'Accept': 'application/octet-stream'},
+               'remote_user': 'test',
+               'trail': ['test', 'rawkey']}
+        rep = {'headers': {}}
+        self.GET(req, rep)
+        self.assertEqual(rep['output'], '1234'.encode('utf-8'))
+
+    def test_9_2_GETKey(self):
+        req = {'remote_user': 'test',
+               'trail': ['test', 'rawkey']}
+        rep = {}
+        self.GET(req, rep)
+        self.assertEqual(json.loads(rep['output']),
+                         {"type": "simple", "value":
+                          b64encode('1234'.encode('utf-8')).decode('utf-8')})

commit a89948e37d78b71ba99e5e3321501be742c3a1b4

Python 3: Fix output encoding
Commit 57686114cfd464bb6b8a2fe6be6d80ea38d43da9 introduced an encoding
bug for Python 3. In Python3 str(b'somebytestr') returns the string
"b'somebytestring'". To fix the issue the output is now converted to
bytes if and only if the handler has returned text. The output wfile
accepts only bytes anyway.

Signed-off-by: Christian Heimes <cheimes@redhat.com>

custodia/httpd/server.py
@@ -321,12 +321,9 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
                 shutil.copyfileobj(output, self.wfile)
                 output.close()
             elif output is not None:
-                ctype = response.get('headers', {}).get('Content-type',
-                                                        'application/json')
-                if ctype == 'application/octet-stream':
-                    self.wfile.write(bytes(output))
-                else:
-                    self.wfile.write(str(output).encode('utf-8'))
+                if not isinstance(output, six.binary_type):
+                    output = output.encode('utf-8')
+                self.wfile.write(output)
             else:
                 self.close_connection = 1
             self.wfile.flush()