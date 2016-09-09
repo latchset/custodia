cc2b76856ed6727724032b3936a46d848401ec33
Christian Heimes <cheimes@redhat.com>
2016-09-09 14:53:16 +0200

    Add --skip-servertests option
    A new options makes it possible to run only unit test and skip
    integration tests. tox -- --skip-servertests skip all integration tests
    that spawn a Custodia server instance.
    
    Closes #72
    Signed-off-by: Christian Heimes <cheimes@redhat.com>
    Reviewed-by: Simo Sorce <simo@redhat.com>
    Closes #74

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,29 @@
 # Copyright (C) 2016  Custodia Project Contributors - see LICENSE file
 import warnings
 
+import pytest
+
 from custodia.log import ProvisionalWarning
 
 # silence our own warnings about provisional APIs
 warnings.simplefilter('ignore', category=ProvisionalWarning)
 # deprecated APIs raise an exception
 warnings.simplefilter('error', category=DeprecationWarning)
+
+
+SKIP_SERVERTEST = "--skip-servertests"
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        SKIP_SERVERTEST,
+        action="store_true",
+        help="Skip integration tests"
+    )
+
+
+def pytest_runtest_setup(item):
+    skip_servertest = item.config.getoption(SKIP_SERVERTEST)
+    if skip_servertest and item.get_marker("servertest") is not None:
+        # args has --skip-servertests and test is marked as servertest
+        pytest.skip("Skip integration test")
