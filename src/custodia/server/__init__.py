1471fc33eeaaf93ca37fcd2ae4ceace981d18fd3
Christian Heimes <cheimes@redhat.com>
2017-05-09 11:21:24 +0200

    Refactoring: Split custodia.server package
    Move argparser and configparser related code to separate modules.
    
    Signed-off-by: Christian Heimes <cheimes@redhat.com>

--- src/custodia/server/__init__.py
+++ src/custodia/server/__init__.py
@@ -1,41 +1,20 @@
 # Copyright (C) 2015  Custodia Project Contributors - see LICENSE file
 from __future__ import absolute_import
 
-import argparse
 import importlib
 import os
-import socket
 
 import pkg_resources
 
 import six
 
 from custodia import log
-from custodia.compat import configparser
-from custodia.compat import url_escape
 from custodia.httpd.server import HTTPServer
+from .args import default_argparser
+from .config import parse_config as _parse_config
 
 
 logger = log.getLogger('custodia')
-CONFIG_SPECIALS = ['authenticators', 'authorizers', 'consumers', 'stores']
-
-
-default_argparser = argparse.ArgumentParser(
-    prog='custodia',
-    description='Custodia server'
-)
-default_argparser.add_argument(
-    '--debug',
-    action='store_true',
-    help='Debug mode'
-)
-default_argparser.add_argument(
-    'configfile',
-    nargs='?',
-    type=argparse.FileType('r'),
-    help='Path to custodia server config',
-    default='/etc/custodia/custodia.conf'
-)
 
 
 def attach_store(typename, plugins, stores):
@@ -97,63 +76,6 @@ def _create_plugin(parser, section, menu):
         hconf.update(parser.items(section))
         hconf.pop('handler')
         return handler(hconf)
-
-
-def _parse_config(args, config):
-    """Parse arguments and create basic configuration
-    """
-    defaults = {
-        # Do not use getfqdn(). Internaly it calls gethostbyaddr which might
-        # perform a DNS query.
-        'hostname': socket.gethostname(),
-    }
-
-    parser = configparser.ConfigParser(
-        interpolation=configparser.ExtendedInterpolation(),
-        defaults=defaults
-    )
-    parser.optionxform = str
-
-    with args.configfile as f:
-        parser.read_file(f)
-
-    for s in CONFIG_SPECIALS:
-        config[s] = dict()
-
-    # add env
-    parser['ENV'] = {
-        k: v.replace('$', '$$') for k, v in os.environ.items()
-        if not set(v).intersection('\r\n\x00')}
-
-    # parse globals first
-    if parser.has_section('global'):
-        for opt, val in parser.items('global'):
-            if opt in CONFIG_SPECIALS:
-                raise ValueError('"%s" is an invalid '
-                                 '[global] option' % opt)
-            config[opt] = val
-
-        config['tls_verify_client'] = parser.getboolean(
-            'global', 'tls_verify_client', fallback=False)
-        config['debug'] = parser.getboolean(
-            'global', 'debug', fallback=False)
-        if args.debug:
-            config['debug'] = True
-        config['auditlog'] = os.path.abspath(
-            config.get('auditlog', 'custodia.audit.log'))
-        config['umask'] = int(config.get('umask', '027'), 8)
-
-        url = config.get('server_url')
-        sock = config.get('server_socket')
-        if bool(url) == bool(sock):
-            raise ValueError("Exactly one of 'server_url' or "
-                             "'server_socket' is required.")
-        if sock:
-            server_socket = os.path.abspath(sock)
-            config['server_url'] = 'http+unix://{}/'.format(
-                url_escape(server_socket, ''))
-
-    return parser
 
 
 def _load_plugins(config, parser):
