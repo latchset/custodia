7dde5b42f3d81a554d553523b5ed30c9ec1c4a7f
Christian Heimes <cheimes@redhat.com>
2017-05-11 09:26:06 +0200

    Release v0.4.1
    Signed-off-by: Christian Heimes <cheimes@redhat.com>

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     name='custodia.ipa',
     description='FreeIPA Vault plugin for Custodia',
     long_description=long_description,
-    version='0.5.dev1',
+    version='0.4.1',
     license='GPLv3+',
     maintainer='Custodia project Contributors',
     maintainer_email='cheimes@redhat.com',
